aaaf0451e80db9bc3deb04960af3ec44dd1339e5
asuka <asuka.altair@gmail.com>
2020-06-13 15:05:28 +0900

    [finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '07e2283cbcfd8474229af9734d498d70a031c12c714fd052634271f50d9c679fffb13b6acf083f54380d93cab68bc5946d5904d9294471a0660b8f08e7d6fa9b'
